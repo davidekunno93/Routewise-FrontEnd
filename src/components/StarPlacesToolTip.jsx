15bfd780226e5fb1aa49606736520afa1401fac1
David Ekunno <matramere@gmail.com>
2024-01-08 22:49:27 -0600

    tooltip modal fix

--- src/components/StarPlacesToolTip.jsx
+++ src/components/StarPlacesToolTip.jsx
@@ -40,17 +40,18 @@ const StarPlacesToolTip = ({ open, currentTrip, onClose }) => {
     return (
         <div className="overlay-placeholder">
             <Fade className='z-99999' duration={200} triggerOnce>
-                <div className="overlay"></div>
-                <div className="toolTip-modal">
-                    <p className="m-0 page-subheading-bold">Tip: Prioritize your must-go places</p>
-                    <p className="m-0 page-text bold500">Star <i>must-go</i> places so we can make sure these are included in your itinerary</p>
-                    <img src="https://i.imgur.com/DLgJ5sS.png" alt="" className='' />
-                    <div className="flx-r gap-3 aligns-l ml15">
-                        <input id='dontShowCheckbox' onClick={() => updateFirstPlaceAdded()} type="checkbox" className='checkbox-medium' />
-                        <p className="m-0 page-text">Don't show this again</p>
-                    </div>
+                <div className="overlay">
+                    <div className="toolTip-modal">
+                        <p className="m-0 page-subheading-bold">Tip: Prioritize your must-go places</p>
+                        <p className="m-0 page-text bold500">Star <i>must-go</i> places so we can make sure these are included in your itinerary</p>
+                        <img src="https://i.imgur.com/DLgJ5sS.png" alt="" className='' />
+                        <div className="flx-r gap-3 aligns-l ml15">
+                            <input id='dontShowCheckbox' onClick={() => updateFirstPlaceAdded()} type="checkbox" className='checkbox-medium' />
+                            <p className="m-0 page-text">Don't show this again</p>
+                        </div>
 
-                    <button onClick={() => onClose()} className="btn-primaryflex">Got it!</button>
+                        <button onClick={() => onClose()} className="btn-primaryflex">Got it!</button>
+                    </div>
                 </div>
             </Fade>
         </div>
