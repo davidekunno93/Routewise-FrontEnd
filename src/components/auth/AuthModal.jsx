f03c87b7ba5072cb13cecf5c5da1b7185608d9a1
David Ekunno <matramere@gmail.com>
2024-05-18 18:49:28 -0500

    updated close button on auth modal

--- src/components/auth/AuthModal.jsx
+++ src/components/auth/AuthModal.jsx
@@ -224,83 +224,89 @@ export const AuthModal = ({ open, authIndex, onClose }) => {
                     <div className="overlay"></div>
                     <div className={`auth-modal flx-r ${mobileModeNarrow && "mobileNarrow"}`}>
                         {!mobileMode &&
-                        <div className="auth-modal-imgDiv">
-                            <div className="caption">
-                                {/* <span className="material-symbols-outlined white-text">location_on</span> */}
-                                <img src="https://i.imgur.com/nTZTn9C.png" alt="" className="img-xxxsmall" />
-                                <p className='m-0 white-text'>Lisbon, Portugal&nbsp;</p>
+                            <div className="auth-modal-imgDiv">
+                                <div className="caption">
+                                    {/* <span className="material-symbols-outlined white-text">location_on</span> */}
+                                    <img src="https://i.imgur.com/nTZTn9C.png" alt="" className="img-xxxsmall" />
+                                    <p className='m-0 white-text'>Lisbon, Portugal&nbsp;</p>
                                 </div>
-                            <img src="https://i.imgur.com/8WWTVLl.png" alt="" className="auth-modal-img" />
-                            {/* <img src="https://i.imgur.com/9HR2xh9.png" alt="" className="auth-modal-img" /> */}
-                        </div>
+                                <img src="https://i.imgur.com/8WWTVLl.png" alt="" className="auth-modal-img" />
+                                {/* <img src="https://i.imgur.com/9HR2xh9.png" alt="" className="auth-modal-img" /> */}
+                            </div>
                         }
                         <CreatePassword showPassword={showPassword} hidePassword={hidePassword} open={createPasswordOpen} email={registerEmail} onClose={() => closeCreatePassword()} closeAll={() => closeAll1()} />
                         <div className={`carousel-window-auth ${mobileMode && "mobile"} ${mobileModeNarrow && "mobileNarrow"} m-auto flx-r position-relative ${createPasswordOpen && "d-none"}`}>
                             <div id='loadingBox' className={`loadingBox-2 z-1000000 w-100 h-100 ${isLoading ? null : "hidden-o"}`}>
                                 <Loading />
                             </div>
-                            <span onClick={onClose} className={`closeBtn-auth ${mobileModeNarrow && "mobileNarrow"} material-symbols-outlined position-absolute xx-large color-gains`}>
-                                close
-                            </span>
+                            {mobileModeNarrow ?
+                                <div onClick={onClose} className="closeBtn-circle abs-vcenter abs-bottom-20">
+                                    <spa className="material-symbols-outlined gains-text">close</spa>
+                                </div>
+                                :
+                                <span onClick={onClose} className={`closeBtn-auth material-symbols-outlined position-absolute xx-large color-gains`}>
+                                    close
+                                </span>
+                            }
                             <div id='inner' className="inner-no-flex" style={{ transform: `translateX(-${activeIndex * 100}%)` }}>
                                 <div className="carousel-item-centered">
-                                <div className={`sign-up-box ${mobileModeNarrow && "mobile"} position-relative ${activeIndex !== 0 && "hidden-o"}`}>
+                                    <div className={`sign-up-box ${mobileModeNarrow && "mobile"} position-relative ${activeIndex !== 0 && "hidden-o"}`}>
 
-                                    <h1 className={`mt-4 ${mobileModeNarrow && "xx-large"}`}>Sign Up</h1>
-                                    <button onClick={() => googleSignIn()} className={`btn-outline ${mobileModeNarrow && "mobile"} bg-white position-relative my-1 font-jakarta purple-text`}><img src="https://i.imgur.com/JN3RsNN.png" alt="" className={`btn-icon-left ${mobileModeNarrow && "mobile"}`} /> Sign up with Google</button>
-                                    {/* <button className='btn-outline bg-white position-relative my-1 font-jakarta purple-text'><img src="https://i.imgur.com/24a8oUQ.png" alt="" className="btn-icon-left" /> Sign up with Facebook</button> */}
-                                    <div className="hr-block w-75 flx-r">
-                                        <div className="flx-1 flx-c just-ce">
-                                            <hr className='w-100 border-gains' />
+                                        <h1 className={`mt-4 ${mobileModeNarrow && "xx-large"}`}>Sign Up</h1>
+                                        <button onClick={() => googleSignIn()} className={`btn-outline ${mobileModeNarrow && "mobile"} bg-white position-relative my-1 font-jakarta purple-text`}><img src="https://i.imgur.com/JN3RsNN.png" alt="" className={`btn-icon-left ${mobileModeNarrow && "mobile"}`} /> Sign up with Google</button>
+                                        {/* <button className='btn-outline bg-white position-relative my-1 font-jakarta purple-text'><img src="https://i.imgur.com/24a8oUQ.png" alt="" className="btn-icon-left" /> Sign up with Facebook</button> */}
+                                        <div className="hr-block w-75 flx-r">
+                                            <div className="flx-1 flx-c just-ce">
+                                                <hr className='w-100 border-gains' />
+                                            </div>
+                                            <p className='px-2'><strong>or</strong></p>
+                                            <div className="flx-1 flx-c just-ce">
+                                                <hr className='w-100 border-gains' />
+                                            </div>
                                         </div>
-                                        <p className='px-2'><strong>or</strong></p>
-                                        <div className="flx-1 flx-c just-ce">
-                                            <hr className='w-100 border-gains' />
+                                        <p id='invalidEmail' className="m-0 red-text o-none">&nbsp;</p>
+                                        <div className={`inputBox ${mobileModeNarrow && "mobile"} flx-c my-2`}>
+                                            <input id='registerEmail' onChange={(e) => updateRegisterEmail(e)} type='text' className={`input-model ${mobileModeNarrow && "mobile"}`} required />
+                                            <span className='title font-jakarta'>Email</span>
                                         </div>
-                                    </div>
-                                    <p id='invalidEmail' className="m-0 red-text o-none">&nbsp;</p>
-                                    <div className={`inputBox ${mobileModeNarrow && "mobile"} flx-c my-2`}>
-                                        <input id='registerEmail' onChange={(e) => updateRegisterEmail(e)} type='text' className={`input-model ${mobileModeNarrow && "mobile"}`} required />
-                                        <span className='title font-jakarta'>Email</span>
-                                    </div>
 
-                                    <button id='continueEmail' onClick={() => continueWithEmail()} className={`btn-primary ${mobileModeNarrow && "mobile"} bg-white font-jakarta bg-lightpurple white-text`}>Continue with email</button>
-                                    <div className='m-0 small mt-3 font-jakarta dark-text'>Already have an account? <Link onClick={() => updateIndex(1)} className='link-text'><strong>Log In</strong></Link></div>
-                                </div>
+                                        <button id='continueEmail' onClick={() => continueWithEmail()} className={`btn-primary ${mobileModeNarrow && "mobile"} bg-white font-jakarta bg-lightpurple white-text`}>Continue with email</button>
+                                        <div className='m-0 small mt-3 font-jakarta dark-text'>Already have an account? <Link onClick={() => updateIndex(1)} className='link-text'><strong>Log In</strong></Link></div>
+                                    </div>
                                 </div>
                                 <div className="carousel-item-centered">
-                                <div className={`sign-in-box ${mobileModeNarrow && "mobile"} ${activeIndex !== 1 && "hidden-o"}`}>
-                                    <h1 className={`mt-4 ${mobileModeNarrow && "xx-large"}`}>Log In</h1>
-                                    <button onClick={() => googleSignIn()} className={`btn-outline ${mobileModeNarrow && "mobile"} bg-white purple-text position-relative my-1 font-jakarta`}><img src="https://i.imgur.com/JN3RsNN.png" alt="" className={`btn-icon-left ${mobileModeNarrow && "mobile"}`} /> Log in with Google</button>
-                                    
-                                    {/* <button target="_blank" className='btn-outline bg-white purple-text position-relative my-1 font-jakarta'><img src="https://i.imgur.com/24a8oUQ.png" alt="" className="btn-icon-left" /> Log in with Facebook</button> */}
-                                    <div className="hr-block w-75 flx-r">
-                                        <div className="flx-1 flx-c just-ce">
-                                            <hr className='w-100 border-gains' />
+                                    <div className={`sign-in-box ${mobileModeNarrow && "mobile"} ${activeIndex !== 1 && "hidden-o"}`}>
+                                        <h1 className={`mt-4 ${mobileModeNarrow && "xx-large"}`}>Log In</h1>
+                                        <button onClick={() => googleSignIn()} className={`btn-outline ${mobileModeNarrow && "mobile"} bg-white purple-text position-relative my-1 font-jakarta`}><img src="https://i.imgur.com/JN3RsNN.png" alt="" className={`btn-icon-left ${mobileModeNarrow && "mobile"}`} /> Log in with Google</button>
+
+                                        {/* <button target="_blank" className='btn-outline bg-white purple-text position-relative my-1 font-jakarta'><img src="https://i.imgur.com/24a8oUQ.png" alt="" className="btn-icon-left" /> Log in with Facebook</button> */}
+                                        <div className="hr-block w-75 flx-r">
+                                            <div className="flx-1 flx-c just-ce">
+                                                <hr className='w-100 border-gains' />
+                                            </div>
+                                            <p className='px-2'><strong>or</strong></p>
+                                            <div className="flx-1 flx-c just-ce">
+                                                <hr className='w-100 border-gains' />
+                                            </div>
                                         </div>
-                                        <p className='px-2'><strong>or</strong></p>
-                                        <div className="flx-1 flx-c just-ce">
-                                            <hr className='w-100 border-gains' />
+
+
+                                        <div className={`inputBox ${mobileModeNarrow && "mobile"} my-2`}>
+                                            <input id='loginEmail' onChange={(e) => updateLoginEmail(e)} type='text' className={`input-model ${mobileModeNarrow && "mobile"}`} required />
+                                            <span className='title font-jakarta'>Email</span>
                                         </div>
+
+                                        <div id='login-password-input' className={`inputBox ${mobileModeNarrow && "mobile"} mb-2`}>
+                                            <input id='loginPassword' onChange={(e) => updateLoginPassword(e)} type='password' className={`input-model ${mobileModeNarrow && "mobile"}`} required />
+                                            <span className='title font-jakarta'>Password</span>
+                                            <div id='loginPasswordOpen' onClick={() => showPassword('loginPassword')} className='icon-right flx-c just-ce'><img src="https://i.imgur.com/DkqcKz7.png" alt="" className="icon-xsmall center o-80" /></div>
+                                            <div id='loginPasswordClose' onClick={() => hidePassword('loginPassword')} className='icon-right flx-c just-ce d-none'><img src="https://i.imgur.com/yoo70zI.png" alt="" className="icon-xsmall center o-80" /></div>
+                                        </div>
+
+                                        <button onClick={() => signInWithEmail()} id='loginWithEmail' className={`btn-primary ${mobileModeNarrow && "mobile"} bg-lightpurple white-text font-jakarta`}>Log in with email</button>
+                                        <div className='m-0 small mt-3 font-jakarta dark-text'>Create an account? <Link onClick={() => updateIndex(0)} className='link-text'><strong>Sign Up</strong></Link></div>
+
                                     </div>
-                                    
-
-                                    <div className={`inputBox ${mobileModeNarrow && "mobile"} my-2`}>
-                                        <input id='loginEmail' onChange={(e) => updateLoginEmail(e)} type='text' className={`input-model ${mobileModeNarrow && "mobile"}`} required />
-                                        <span className='title font-jakarta'>Email</span>
-                                    </div>
-
-                                    <div id='login-password-input' className={`inputBox ${mobileModeNarrow && "mobile"} mb-2`}>
-                                        <input id='loginPassword' onChange={(e) => updateLoginPassword(e)} type='password' className={`input-model ${mobileModeNarrow && "mobile"}`} required />
-                                        <span className='title font-jakarta'>Password</span>
-                                        <div id='loginPasswordOpen' onClick={() => showPassword('loginPassword')} className='icon-right flx-c just-ce'><img src="https://i.imgur.com/DkqcKz7.png" alt="" className="icon-xsmall center o-80" /></div>
-                                        <div id='loginPasswordClose' onClick={() => hidePassword('loginPassword')} className='icon-right flx-c just-ce d-none'><img src="https://i.imgur.com/yoo70zI.png" alt="" className="icon-xsmall center o-80" /></div>
-                                    </div>
-
-                                    <button onClick={() => signInWithEmail()} id='loginWithEmail' className={`btn-primary ${mobileModeNarrow && "mobile"} bg-lightpurple white-text font-jakarta`}>Log in with email</button>
-                                    <div className='m-0 small mt-3 font-jakarta dark-text'>Create an account? <Link onClick={() => updateIndex(0)} className='link-text'><strong>Sign Up</strong></Link></div>
-
-                                </div>
                                 </div>
                             </div>
                         </div>
